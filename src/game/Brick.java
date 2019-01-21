236ebf86e04889b4d38d036d0217bcc5b77901e7
Januario Carreiro <jjc70@duke.edu>
2019-01-21 00:34:39 -0500

    Added comments to Brick and PowerUp classes

--- src/game/Brick.java
+++ src/game/Brick.java
@@ -3,22 +3,47 @@ package game;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * @author Januario Carreiro
+ * Use this class to create new Brick objects in-game.
+ * e.g. Brick brick = new Brick(0, 0, 50, 50) creates a sqaure Brick at (0, 0) where the
+ * width and height of the brick are both 50 pixels.
+ * Brick's color is determined by its health
+ * Health of brick when using setHealth() should be between 1 and 5. May also use object.setFill() in
+ * GameLoop class if necessary.
+ * Permanent Brick objects are not permanent per-se, but should not count towards number of
+ * bricks needed to be eliminated in a level as they have 1000 health points.
+ */
 public class Brick extends Rectangle {
     boolean destroyed;
     int health;
 
+    /**
+     * Default Constructor
+     * Use when you want to remove a Brick object from game
+     */
     public Brick() {
         super(0, 0, 0, 0);
         health = 0;
         destroyed = true;
     }
 
+    /**
+     * Constructor used to make new Bricks. Need to use setHealth() to set health.
+     * @param x location x-axis
+     * @param y location y-axis
+     * @param width width of block
+     * @param height height of block
+     */
     public Brick(double x, double y, double width, double height) {
         super(x, y, width, height);
         health = 1;
         destroyed = false;
     }
 
+    /**
+     * Will reduce health by 1 point. Will also change color of brick to match health.
+     */
     public void reduceHealth() {
         health--;
         switch(health) {
@@ -46,6 +71,10 @@ public class Brick extends Rectangle {
         }
     }
 
+    /**
+     * Sets health of a Brick object. This also determines the Brick object's color if health between 1 and 5.
+     * @param value is the number of health points.
+     */
     public void setHealth(int value) {
         health = value;
         switch(health) {
@@ -67,14 +96,23 @@ public class Brick extends Rectangle {
         }
     }
 
+    /**
+     * Sets instance variable destroyed to true.
+     */
     public void setDestroyed() {
         destroyed = true;
     }
 
+    /**
+     * @return value of destroyed.
+     */
     public boolean isDestroyed() {
         return destroyed;
     }
 
+    /**
+     * Makes a gray Brick Object with 1000 health points.
+     */
     public void setPermanent() {
         health = 1000;
         super.setFill(Color.GRAY);
--- src/game/PowerUp.java
+++ src/game/PowerUp.java
@@ -4,20 +4,39 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import java.util.Random;
 
-// GREEN MORE LIVES
-// YELLOW EXTENDS PLATFORM
-// BLUE FAST BALL
-// ORANGE REDUCES PLATFORM
-// RED LOSE GAME
-
+/**
+ * @author Januario Carreiro
+ * Use this class to create new PowerUp objects in-game.
+ * e.g. PowerUp(5, 2) creates a PowerUp object with a radius of 5 with a 50% chance of being
+ * Green and a 50% chance of being Yellow.
+ * If we do PowerUp(5, 5), the PowerUp object has a 20% of being either Green, Yellow, Blue,
+ * Orange, or Red.
+ * DO NOT let the second parameter be equal to or greater than 6.
+ * What each color will mean in GameLoop:
+ * GREEN:   LIVES + 1
+ * YELLOW:  EXTENDS PLATFORM
+ * BLUE:    FASTER BALL
+ * ORANGE:  REDUCES PLATFORM
+ * RED:     GAME OVER
+ */
 public class PowerUp extends Circle {
     Color myColor;
 
+    /**
+     * Default constructor. Use this after removing PowerUp object from root in order to keep PowerUp
+     * object from activating more than once.
+     */
     public PowerUp() {
         super(0);
         myColor = Color.BLACK;
     }
 
+    /**
+     * Constructor used to make new PowerUp objects. Randomly determines which color it will be
+     * based on the level given.
+     * @param radius is the radius of the circle.
+     * @param level is the current difficulty level. Should not be greater than 5.
+     */
     public PowerUp(int radius, int level) {
         super(radius);
         Random rand = new Random();
